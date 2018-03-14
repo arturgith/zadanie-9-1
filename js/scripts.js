3dfa72564b1045bc4eb905195456f4976e4f769c
Artur Marcinkowski <arturmarcinkowski@interia.pl>
2018-03-14 21:41:47 +0100

    Change scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 // scripts.js
-var triangle1Area = getTriangleArea(10, 15);
-var triangleArea = getTriangleArea(10, 5);
-var triangle2Area = getTriangleArea(10, 2);
-
 function getTriangleArea(a, h) {
     if (a<=0 || h<=0) {
-    	console.log("Nieprawidłowe dane");
+    	return "Nieprawidłowe dane";
     } else {
-		return('Pole trójkta wynosi '+ a * h / 2); 
+		return a * h / 2; 
     }	
     }
-console.log(getTriangleArea(10, 0))
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangleArea = getTriangleArea(10, 5);
+var triangle2Area = getTriangleArea(10, 0);
+console.log(getTriangleArea(10, 6))
 console.log(triangle1Area)
 console.log(triangleArea)
 console.log(triangle2Area)
